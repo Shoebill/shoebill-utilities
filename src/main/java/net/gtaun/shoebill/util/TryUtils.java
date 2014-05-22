e1d2e215eb050b9456265cc52bd69558bf55ffc2
mk124 <mk124a@gmail.com>
2014-05-23 05:22:44 +0800

    TryUtils added

---
+++ src/main/java/net/gtaun/shoebill/util/TryUtils.java
@@ -0,0 +1,33 @@
+package net.gtaun.shoebill.util;
+
+import java.util.function.Consumer;
+
+public class TryUtils
+{
+	public interface ThrowableFunction
+	{
+		void call() throws Throwable;
+	}
+
+	public static void tryTo(ThrowableFunction func)
+	{
+		tryTo(func, (ex) -> ex.printStackTrace());
+	}
+
+	public static void tryTo(ThrowableFunction func, Consumer<Throwable> exceptionHandler)
+	{
+		try
+		{
+			func.call();
+		}
+		catch (Throwable ex)
+		{
+			exceptionHandler.accept(ex);
+		}
+	}
+
+	public static Consumer<ThrowableFunction> tryToFunc(Consumer<Throwable> exceptionHandler)
+	{
+		return (func) -> tryTo(func, exceptionHandler);
+	}
+}
